874372257dec0c2c5a7495c43aa718b7fb0b0e6b
saifvelly <mohammed.kssim20022002@gmail.com>
2025-01-24 14:46:44 +0100

    added user entity

--- src/main/java/com/auth/jwtauth/user/User.java
+++ src/main/java/com/auth/jwtauth/user/User.java
@@ -1,4 +1,27 @@
 package com.auth.jwtauth.user;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Entity
+@Table(name="user")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
 }
