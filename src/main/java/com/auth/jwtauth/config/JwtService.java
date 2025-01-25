a02ccc809a45ac8653380001c7b7757494a9eb92
saifvelly <mohammed.kssim20022002@gmail.com>
2025-01-25 07:41:19 +0100

    implemented extractClaim

--- src/main/java/com/auth/jwtauth/config/JwtService.java
+++ src/main/java/com/auth/jwtauth/config/JwtService.java
@@ -6,13 +6,25 @@ import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
     private static final String SECRET_KEY="0f4bf5d0cc18f5a91304b981aa16e23e59b9f77c3a5e90846e007667ad97599b";
     public String extractUsername(String token) {
-        return null; // just for the moment
+        return extractClaim(token, Claims::getSubject); // just for the moment
     }
+
+
+    //implemet the extractClaim method
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver){
+        //FIrst let's get all the claims
+        final Claims claims = extractAllClaims(token);
+        //extracting any claim
+        return claimsResolver.apply(claims);
+    }
+
+
 
 
     //extract all the claims
